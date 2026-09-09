e23c8d7fdb875d66b152e03a37b785481df5f452
jkridner <>
2026-09-09 18:25:15 +0000

    Xcode Cloud: build FossilCore.xcframework chain in ci_post_clone
    FossilOrigin-Name: 3cc4006a4aa2074e9d23599f6d5135c292deedf4f4313d72244e18fe0d11d85b

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+scripts/fetch-fossil.sh
+scripts/gen-fossil-sources.sh
+scripts/build-libressl-ios.sh
+scripts/build-fossil-xcframework.sh
+test -d ios/Frameworks/FossilCore.xcframework
