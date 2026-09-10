64560c75746c047cec679de0014ea885f140c23a
jkridner <>
2026-09-10 02:15:07 +0000

    ci_post_clone: fetch LibreSSL source before building it
    FossilOrigin-Name: 4f98dedcf8d70b203f8955f52084f51b2a6b9708018d6f01ac0bc38525688a8e

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,6 +3,7 @@ set -e
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 scripts/fetch-fossil.sh
 scripts/gen-fossil-sources.sh
+scripts/fetch-libressl.sh
 scripts/build-libressl-ios.sh
 scripts/build-fossil-xcframework.sh
 test -d ios/Frameworks/FossilCore.xcframework
